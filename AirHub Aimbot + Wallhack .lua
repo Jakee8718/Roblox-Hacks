51d3266f48c4ceb81328f8455a1cf7876840f5a3
Jake <112051772+Jakee8718@users.noreply.github.com>
2023-06-21 21:35:08 -0400

    cred

---
+++ AirHub Aimbot + Wallhack .lua
@@ -0,0 +1,5 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/AirHub/main/AirHub.lua"))()
+
+-- made by https://github.com/Exunys/AirHub
+-- added to the list for simplicity.
+-- Universal aimbot + wallhack etc.
