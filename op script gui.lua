fc30e00258067b4e2b12ba7890ffbea9e66dc452
Jake <112051772+Jakee8718@users.noreply.github.com>
2023-06-21 21:28:43 -0400

    Create op script gui.lua

---
+++ op script gui.lua
@@ -0,0 +1,8 @@
+getgenv().Key = "Key1203"
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/V.G-Hub/main/V.Ghub'))()
+-- see thread for games and discord or when key updates
+-- https://v3rmillion.net/showthread.php?tid=1127420
+
+-- explanation on script:
+-- basically when you join a game and run this script it will automatically detect what game you are in. It has universal hacks but also pairs hacks with the game you are playing.
